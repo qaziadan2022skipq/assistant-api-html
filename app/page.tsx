1f468e3e7b8defad1254cb4ef0e124c0132db56b
Qazi Adan Hidayat <qaziadan.hidayat.skipq@gmail.com>
2025-02-06 21:56:24 +0500

    added the steaming in chabot

--- app/page.tsx
+++ app/page.tsx
@@ -1,77 +1,23 @@
 "use client";
 
-import * as z from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { formSchema } from "./constants";
+import { Message, useAssistant } from "ai/react";
 import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
-import axios from "axios";
 import { cn } from "@/lib/utils";
-import useMessageStore from "@/hooks/message-store";
 import { UserAvatar } from "@/components/user-avatar";
 import { BotAvatar } from "@/components/bot-avatar";
-import { useToast } from "@/hooks/use-toast";
 import { Empty } from "@/components/empty";
-import { Loader } from "@/components/loader";
 import Heading from "@/components/heading";
-import { BotIcon, SproutIcon } from "lucide-react";
-import { useEffect } from "react";
+import { BotIcon } from "lucide-react";
 
 const Conversation = () => {
-  const router = useRouter();
-  const messages = useMessageStore();
-  const { toast } = useToast();
-
-
-  useEffect(() => {
-    // create new thread
-    localStorage.setItem("OpenAIAssistantThreadId", "");
-    createThread();
-    
-  }, []);
-
-  const createThread = async () => {
-    const response = await axios.post("/api/create-thread");
-    localStorage.setItem("OpenAIAssistantThreadId", response.data);
-    console.log(response.data)
-  };
-
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      prompt: "",
-    },
-  });
-
-  const isLoading = form.formState.isSubmitting;
-  const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    try {
-      messages.addMessage({ role: "user", message_content: values.prompt });
-      const botAnswer = await axios.post("/api/openai-assistant-bot", {
-        userMessage: JSON.stringify(values.prompt),
-        threadId: localStorage.getItem("OpenAIAssistantThreadId"),
-      });
-        console.log(botAnswer.data.text.value)
-      messages.addMessage({
-        role: "bot",
-        message_content: botAnswer.data.text.value,
-      });
-      form.reset();
-    } catch (error: any) {
-      console.log(error);
-    } finally {
-      router.refresh();
-    }
-  };
+  const { status, messages, input, submitMessage, handleInputChange } =
+    useAssistant({ api: "/api/assistant" });
 
   return (
     <div className="flex flex-col min-h-screen border rounded-xl pb-1 shadow-md bg-white">
       <Heading
         title="Your Assistant"
-        description="Chat with more powerful assistant"
+        description="Chat with a more powerful assistant"
         icon={BotIcon}
         iconColor="text-sky-500"
         bgColor="bg-sky-500/10"
@@ -79,78 +25,100 @@ const Conversation = () => {
       <div className="flex-1 px-4 lg:px-8 overflow-auto">
         <div className="space-y-4 mt-4">
           <div className="flex flex-col gap-y-4">
-            {messages.messages.map((message, index) => (
+            {messages.map((m: Message) => (
               <div
-                key={index}
+                key={m.id}
                 className={cn(
                   "p-6 w-full flex items-start gap-x-8 rounded-lg",
-                  message.role === "user"
+                  m.role === "user"
                     ? "bg-white border border-black/10"
                     : "bg-muted border border-black/20"
                 )}
               >
-                {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
+                {m.role === "user" ? <UserAvatar /> : <BotAvatar />}
                 <p className="text-sm whitespace-pre-wrap">
-                  {String(message.message_content)}
+                  {m.role !== "data" && m.content}
                 </p>
               </div>
             ))}
           </div>
-          {isLoading && (
-            <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
-              <Loader />
-            </div>
-          )}
-          {messages.messages.length === 0 && !isLoading && (
-            <Empty label="Chat With Assistant" />
-          )}
+
+          {messages.length === 0 && <Empty label="Chat With Assistant" />}
         </div>
       </div>
-      <div id="Message" className="px-4 lg:px-8">
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="
-              rounded-lg 
-              border 
-              w-full 
-              p-4 
-              px-3 
-              md:px-6 
-              focus-within:shadow-sm
-              grid
-              grid-cols-12
-              gap-2
-            "
+      <div id="Message" className="flex items-center px-4 lg:px-8">
+        <form
+          onSubmit={submitMessage}
+          className="
+        rounded-lg 
+        border 
+        w-full 
+        p-4 
+        px-3 
+        md:px-6 
+        focus-within:shadow-sm
+        grid
+        grid-cols-12
+        gap-2
+      "
+        >
+          <input
+            disabled={status !== "awaiting_message"}
+            value={input}
+            placeholder="What is the temperature in the living room?"
+            onChange={handleInputChange}
+            className="col-span-12 lg:col-span-10"
+          />
+          <Button
+            className="col-span-12 lg:col-span-2 w-full bg-sky-500"
+            type="submit"
           >
-            <FormField
-              name="prompt"
-              render={({ field }) => (
-                <FormItem className="col-span-12 lg:col-span-10">
-                  <FormControl className="m-0 p-0">
-                    <Input
-                      className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
-                      disabled={isLoading}
-                      placeholder="How can I help you today?"
-                      {...field}
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <Button
-              className="col-span-12 lg:col-span-2 w-full bg-sky-500"
-              type="submit"
-              disabled={isLoading}
-              size="icon"
-            >
-              Send
-            </Button>
-          </form>
-        </Form>
+            Send
+          </Button>
+        </form>
       </div>
     </div>
   );
 };
 
 export default Conversation;
+
+// 'use client';
+
+// import { Message, useAssistant } from 'ai/react';
+
+// export default function Chat() {
+//   const { status, messages, input, submitMessage, handleInputChange } =
+//     useAssistant({ api: '/api/assistant' });
+
+//   return (
+//     <div>
+//       {messages.map((m: Message) => (
+//         <div key={m.id}>
+//           <strong>{`${m.role}: `}</strong>
+// {m.role !== 'data' && m.content}
+// {m.role === 'data' && (
+//   <>
+//     {(m.data as any).description}
+//     <br />
+//     <pre className={'bg-gray-200'}>
+//       {JSON.stringify(m.data, null, 2)}
+//     </pre>
+//             </>
+//           )}
+//         </div>
+//       ))}
+
+//       {status === 'in_progress' && <div />}
+
+//       <form onSubmit={submitMessage}>
+//         <input
+//           disabled={status !== 'awaiting_message'}
+//           value={input}
+//           placeholder="What is the temperature in the living room?"
+//           onChange={handleInputChange}
+//         />
+//       </form>
+//     </div>
+//   );
+// }
